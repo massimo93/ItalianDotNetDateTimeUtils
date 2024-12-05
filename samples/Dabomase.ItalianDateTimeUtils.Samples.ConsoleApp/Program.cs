b33e8f4cfdf74e553c1bd810fdaca8401c4434ee
Massimo Serra <massimosrr@gmail.com>
2024-12-05 23:18:22 +0100

    Add first sample app to showcase library methods simple usages

---
+++ samples/Dabomase.ItalianDateTimeUtils.Samples.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Dabomase.ItalianDateTimeUtils;
+
+Console.WriteLine("\n********************************************************");
+Console.WriteLine($"*** {Assembly.GetExecutingAssembly().GetName().Name} ***");
+Console.WriteLine("********************************************************\n");
+
+//IsHoliday method usage samples
+var isTodayItalianHoliday = ItalianHolidaysUtils.IsHoliday(DateTime.UtcNow);
+var wasLastNovemberTheFirstItalianHoliday = ItalianHolidaysUtils.IsHoliday(new DateTime(2024, 11, 1));
+
+//Console printouts
+Console.WriteLine($"{nameof(isTodayItalianHoliday)}: {isTodayItalianHoliday}");
+Console.WriteLine($"{nameof(wasLastNovemberTheFirstItalianHoliday)}: {wasLastNovemberTheFirstItalianHoliday}");
